bf95a5bd9696f42103d9fa189c43687ab44f3a17
sisdream <sissipascale2@gmail.com>
2024-02-29 16:50:29 +0100

    feat : array_filter

--- backend/index.php
+++ backend/index.php
@@ -1,2 +1,22 @@
 <?php
-echo "test di prova ";
+
+$json_list_content = file_get_contents("./todolist.json");
+
+$todolist_array = json_decode($json_list_content);
+
+var_dump($todolist_array);
+
+$todolist_array = array_filter($todolist_array, fn($item) => !empty ($item));
+
+var_dump($todolist_array);
+
+
+
+
+
+
+
+
+// header("Content-Type: application/json");
+
+// echo $json_list_content;
